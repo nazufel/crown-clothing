3f3e2efa135659786554e1d2189c20fe0ccb5544
nazufel <nazufel@gmail.com>
2020-11-11 22:15:43 -0500

    routing set up and stubbing in new pages.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import React from 'react';
+import { Route } from 'react-router-dom'
+import { Switch } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component'
 
+const HatsPage = () => (
+  <div>
+    <h1> HATS PAGE </h1>
+  </div>
+)
+
+const SneakersPage = () => (
+  <div>
+    <h1> SNEAKERS PAGE </h1>
+  </div>
+)
+
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={ HomePage }/>
+        <Route exact path='/hats' component={ HatsPage }/>
+        <Route exact path='/sneakers' component={ SneakersPage }/>
+      </Switch>
     </div>
   );
 }
